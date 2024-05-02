3a8ec2171d173a4806f9b39ec101209b6d9d6220
Varun Balani <varunprahladb@gmail.com>
2024-05-02 18:53:42 +0530

    create podcast generation route

--- app/api/create/route.ts
+++ app/api/create/route.ts
@@ -1,3 +1,214 @@
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+import { sql } from "@vercel/postgres";
+
 export const maxDuration = 300;
 
-export async function POST(request: Request) {}
+const storePodcastEpisodesDeclaration = {
+  name: "storePodcastEpisodes",
+  parameters: {
+    type: "OBJECT",
+    description:
+      "Store the podcast episodes when given a list of episodes with a title and description.",
+    properties: {
+      episodes: {
+        type: "ARRAY",
+        description:
+          "The list of episodes, each having a title and a description.",
+        properties: {
+          title: {
+            type: "STRING",
+            description: "The title of the episode.",
+          },
+          description: {
+            type: "STRING",
+            description: "The description of the episode.",
+          },
+        },
+      },
+    },
+    required: ["episodes"],
+  },
+};
+
+const storePodcastEpisodeScriptsDeclaration = {
+  name: "storePodcastEpisodeScripts",
+  parameters: {
+    type: "OBJECT",
+    description:
+      "Store the podcast episode scripts when given a list of episode numbers and scripts.",
+    properties: {
+      episodes: {
+        type: "ARRAY",
+        description: "The list of episodes, each having a number and a script.",
+        properties: {
+          title: {
+            type: "NUMBER",
+            description: "The number of the episode.",
+          },
+          script: {
+            type: "STRING",
+            description: "The script of the episode.",
+          },
+        },
+      },
+    },
+    required: ["episodes"],
+  },
+};
+
+async function generatePodcastEpisodes(
+  title: string,
+  description: string,
+  style: string,
+  language: string,
+  number_of_episodes: number
+) {
+  const podcast_details = `The podcast is called ${title}. The description is as follows: ${description}. The style of the podcast is ${style}. The podcast episode titles and descriptions should be in ${language}.`;
+
+  const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+  const model = genAI.getGenerativeModel({
+    model: "gemini-pro",
+    tools: { functionDeclarations: [storePodcastEpisodesDeclaration] },
+  });
+  const chat = model.startChat();
+
+  const prompt =
+    `Create a list of ${number_of_episodes} podcast episodes with titles and descriptions for the podcast described below.
+   The episodes should be broken down in such a way that they cover all the content asked for in the podcast
+   description across the course of the episodes. The episodes should be cohesive in structure and have a story-like
+   flow. The episode titles should sound interesting and engaging but not too long.
+   The description of the episode should contain the main points that the episode covers in a concise manner.
+   After you're done, store the list of episodes by passing them into the tool storePodcastEpisodes.
+   The podcast details are as follows: ` + podcast_details;
+
+  console.log(prompt);
+
+  const result = await chat.sendMessage(prompt);
+  console.log("%j", result);
+
+  const call = result.response.functionCalls()[0];
+  if (call === null) return null;
+
+  return call.args.episodes;
+}
+
+async function generateEpisodeScript(
+  podcast_title: string,
+  title: string,
+  description: string,
+  style: string,
+  language: string,
+  avg_duration: string,
+  episode_number: number,
+  prev_episode_description: string
+) {
+  const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+  const model = genAI.getGenerativeModel({
+    model: "gemini-1.5-pro-latest",
+    generationConfig: {
+      maxOutputTokens: 8000,
+    },
+    systemInstruction: `You are a podcast script generating AI. The script should be in a conversational tone.
+    Give a 1-2 line gist of what the previous episode talked about and then continue with the introduction to this episode.
+    The introduction should give a brief overview of what the episode will cover. After the introduction, continue with the main content
+    of the episode. The main content of the episode should cover the points in the episodes description. Elaborate
+    on each point, and structure it in a cohesive, story-like manner. The episode script should be long and informative.
+    The script should be engaging, interesting and in the style of the podcast. Do not use any additional formatting in the episode script. Just output the raw script itself, as it will be read
+    out by a text-to-speech bot. Do not use any section breaks, it should be a continuous, free flowing script. 
+    At the end of the script, include a short outro and ask them to listen to the next episode for more. The script should be in
+    the language that the user requests.`,
+  });
+  const chat = model.startChat();
+
+  const prompt = `Create a script for episode ${episode_number} of the podcast ${podcast_title}. The episodes title is ${title}.
+  The previous episodes' description is as follows: ${prev_episode_description}.
+    This episode should cover the following content: ${description}. The style of 
+    the podcast is ${style}. The episode script should be in ${language}. `;
+
+  console.log(prompt);
+
+  const result = await chat.sendMessage(prompt);
+  console.log("%j", result);
+  if (result.response === null) return null;
+  if (result.response.candidates === null) return null;
+  return result.response.candidates[0].content.parts[0].text;
+}
+
+export async function POST(request: Request) {
+  const body = await request.json();
+  const { userId } = auth();
+
+  if (!userId) {
+    return new NextResponse("Unauthorized", { status: 401 });
+  }
+
+  const {
+    title,
+    description,
+    style,
+    language,
+    number_of_episodes,
+    avg_duration,
+  } = body;
+
+  let podcast_id;
+  try {
+    const { rows } =
+      await sql`INSERT INTO Podcasts (user_id, title) VALUES (${userId}, ${title}) RETURNING podcast_id;`;
+    console.log(rows);
+    podcast_id = rows[0].podcast_id;
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+  console.log(podcast_id);
+
+  const episodes = await generatePodcastEpisodes(
+    title,
+    description,
+    style,
+    language,
+    number_of_episodes
+  );
+  console.log(episodes);
+  if (episodes === null) {
+    return new NextResponse("Error generating episodes", { status: 500 });
+  }
+  let episode_ids = [];
+  for (const [i, episode] of episodes.entries()) {
+    const { rows } = await sql`
+    INSERT INTO Episodes (podcast_id, title, description, episode_number)
+    VALUES (${podcast_id}, ${episode.title}, ${episode.description}, ${i + 1})
+    RETURNING episode_id;
+  `;
+    episode_ids.push(rows[0].episode_id);
+  }
+  console.log(episode_ids);
+  let episode_index = 0;
+  for (const episode_id of episode_ids) {
+    const script = await generateEpisodeScript(
+      title,
+      episodes[episode_index].title,
+      episodes[episode_index].description,
+      style,
+      language,
+      avg_duration,
+      episode_index + 1,
+      episode_index === 0
+        ? "There is no previous episode as this is the first episode of the podcast. Instead, welcome users to the podcast " +
+            title
+        : episodes[episode_index - 1].description
+    );
+    console.log(script);
+    await sql`UPDATE Episodes SET script = ${script} WHERE episode_id = ${episode_id}`;
+    episode_index++;
+    if (script === null) {
+      return new NextResponse("Error generating script", { status: 500 });
+    }
+  }
+  return NextResponse.json({
+    message: "Successfully generated podcast",
+    status: 200,
+  });
+}
--- components/CreateForm.tsx
+++ components/CreateForm.tsx
@@ -6,7 +6,6 @@ import { z } from "zod";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -22,6 +21,8 @@ import {
 } from "@/components/ui/select";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
+import { useState } from "react";
+import { MultiStepLoader } from "@/components/ui/MultiStepLoader";
 
 const formSchema = z.object({
   title: z.string().min(3, {
@@ -40,7 +41,72 @@ const formSchema = z.object({
   episode_duration: z.enum(["short", "medium", "long"]),
 });
 
+type Episode = {
+  title: string;
+  description: string;
+};
+
+const LoadingStates = [
+  {
+    text: "Starting podcast generation",
+  },
+  {
+    text: "Generating podcast overview",
+  },
+  {
+    text: "Planning podcast episodes",
+  },
+  {
+    text: "Creating episode scripts",
+  },
+  {
+    text: "Editing episode scripts",
+  },
+  {
+    text: "Reviewing pilot episode",
+  },
+  {
+    text: "Reviewing middle episodes",
+  },
+  {
+    text: "Reviewing final episode",
+  },
+  {
+    text: "Finalizing episode scripts",
+  },
+  {
+    text: "Uploading episode scripts",
+  },
+  {
+    text: "Publishing podcast",
+  },
+  {
+    text: "Podcast generation complete",
+  },
+  {
+    text: "Running final checks...this might take a while",
+  },
+];
+
 export function CreateForm() {
+  const [generatingEpisodes, setGeneratingEpisodes] = useState(false);
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+    setGeneratingEpisodes(true);
+    const res = await fetch("/api/create", {
+      method: "POST",
+      body: JSON.stringify(values),
+    });
+    const data = await res.json();
+    if (data.status !== 200) {
+      console.log(data);
+      alert("Error creating episodes");
+      return;
+    }
+    setGeneratingEpisodes(false);
+  }
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -54,130 +120,141 @@ export function CreateForm() {
     },
   });
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values);
-  }
-
   return (
-    <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="space-y-4 w-2/3 h-2/3 border-2 border-solid border-zinc-800 p-4"
-      >
-        <FormField
-          control={form.control}
-          name="title"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Title</FormLabel>
-              <FormControl>
-                <Input placeholder="Unveiling the Cosmos" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="description"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Description</FormLabel>
-              <Textarea
-                placeholder="A podcast about the creation of the universe, the big bang, and more"
-                className="resize-none"
-                {...field}
-              />
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="style"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Style</FormLabel>
-              <FormControl>
-                <Input
-                  placeholder="captivating, humorous and informative"
-                  {...field}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <div className="flex justify-between items-center">
+    <div className="w-2/3 h-2/3 flex items-center justify-center">
+      <MultiStepLoader
+        loadingStates={LoadingStates}
+        loading={generatingEpisodes}
+        duration={8000}
+        loop={false}
+      />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-4 w-full h-full border-2 border-solid border-zinc-800 p-4"
+        >
           <FormField
             control={form.control}
-            name="language"
+            name="title"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>Language</FormLabel>
-                <Select
-                  onValueChange={field.onChange}
-                  defaultValue={field.value}
-                >
-                  <SelectTrigger className="w-[180px]">
-                    <SelectValue placeholder="Select a language" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectItem value="english">English</SelectItem>
-                    <SelectItem value="spanish">Spanish</SelectItem>
-                    <SelectItem value="french">French</SelectItem>
-                  </SelectContent>
-                </Select>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="episode_duration"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Episode Duration</FormLabel>
-                <Select
-                  onValueChange={field.onChange}
-                  defaultValue={field.value}
-                >
-                  <SelectTrigger className="w-[180px]">
-                    <SelectValue placeholder="Select the episode duration" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectItem value="short">Short</SelectItem>
-                    <SelectItem value="medium">Medium</SelectItem>
-                    <SelectItem value="long">Long</SelectItem>
-                  </SelectContent>
-                </Select>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="number_of_episodes"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Number of Episodes</FormLabel>
+                <FormLabel>Title</FormLabel>
                 <FormControl>
-                  <Input placeholder="6" {...field} />
+                  <Input placeholder="Unveiling the Cosmos" {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
             )}
           />
-        </div>
-        <div className="flex justify-center items-center">
-          <Button
-            type="submit"
-            className="rounded-xl inline-flex h-12 animate-shimmer items-center justify-center border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-8 font-medium text-slate-100 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
-          >
-            Create Podcast
-          </Button>
-        </div>
-      </form>
-    </Form>
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Description</FormLabel>
+                <Textarea
+                  placeholder="A podcast about the creation of the universe, the big bang, and more"
+                  className="resize-none"
+                  {...field}
+                />
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="style"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Style</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="captivating, humorous and informative"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="flex justify-between items-center">
+            <FormField
+              control={form.control}
+              name="language"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Language</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <SelectTrigger className="w-[180px]">
+                      <SelectValue placeholder="Select a language" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="english">English</SelectItem>
+                      <SelectItem value="spanish">Spanish</SelectItem>
+                      <SelectItem value="french">French</SelectItem>
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="episode_duration"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Episode Duration</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <SelectTrigger className="w-[180px]">
+                      <SelectValue placeholder="Select the episode duration" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="short">Short</SelectItem>
+                      <SelectItem value="medium">Medium</SelectItem>
+                      <SelectItem value="long">Long</SelectItem>
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="number_of_episodes"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Number of Episodes</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="6"
+                      type="number"
+                      {...field}
+                      onChange={(event) =>
+                        field.onChange(Number(event.target.value))
+                      }
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="flex justify-center items-center">
+            <Button
+              type="submit"
+              className="rounded-xl inline-flex h-12 animate-shimmer items-center justify-center border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-8 font-medium text-slate-100 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+            >
+              Create Podcast
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </div>
   );
 }
